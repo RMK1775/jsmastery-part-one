b249c82281cd653e757628126ba230a8e62b3b71
RMKendall <ryan.morris.kendall@gmail.com>
2021-02-18 18:15:26 -0600

    Complete Control flow exercises

--- control-flow.js
+++ control-flow.js
@@ -155,3 +155,106 @@ function showNumbers(input){
      }
 }
 showNumbers(10);
+
+//Falsy: the following return false in JS
+/*
+undefined
+null
+0
+false
+''
+NaN
+*/
+//Anything else is returned as 'truthy'; true
+const array = [0, null, undefined, '', 2, 3];
+function countTruthy(array){
+    let count = 0;
+    for (let value of array){
+        if (value)
+            count++;
+    }
+    return count;
+}
+countTruthy(array);
+
+const movie = {
+    title: 'a',
+    releaseYear: 1986,
+    rating: 4,
+    director: 'b'
+}
+function showStrings(obj){
+    for (let key in obj){
+        if(typeof obj[key] === 'string'){
+        console.log(key, obj[key]);
+        }
+    }
+}
+showStrings(movie);
+
+//For multiples of 3 and 5
+function sum(limit){
+    let sum = 0;
+    for(let i = 1; i <= limit; i++){
+        if (i % 5 == 0 || i % 3 == 0){
+            sum += i;
+        }
+    }
+    return sum;
+}
+sum(10);
+
+const marks = [80, 80, 50];
+/*
+A: 90-100
+B: 80-89
+C: 70-79
+D: 60-69
+F: 0-59
+ */
+function calculateGrade(marks){
+    let average = 0;
+    for(let mark of marks){
+        average += mark;
+    }
+    average = average / marks.length;
+    if(average < 60){
+        return 'F';
+    }else if (average >= 60 && average <= 69){
+        return 'D';
+    }else if (average >= 70 && average <= 79){
+        return 'C';
+    }else if (average >= 80 && average <= 89){
+        return 'B';
+    }else if (average >= 90){
+        return 'A';
+    }
+}
+console.log(calculateGrade(marks));
+
+function showStars(rows){
+    let stars = '';
+   for(let i = 1; i <= rows; i++){
+       stars += '*';
+       console.log(stars);
+   }
+}
+showStars(5);
+
+//Prime: those #s divisible only by 1 and themselves
+function showPrimes(limit){
+    for (let i = 2; i<= limit; i++){
+        if(isPrime(i)){
+            console.log(i);
+        }
+    }
+}
+function isPrime(num){
+    for(let i = 2; i < num; i++){
+        if(num % i == 0){
+            return false;
+        }
+    }
+    return true;
+}
+showPrimes(20);
